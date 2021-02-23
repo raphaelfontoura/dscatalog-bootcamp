ed18308dab39909cd69ae5af7adbf1dba3594a5d
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-02-22 23:05:14 -0300

    Project created

---
+++ backend/src/test/java/com/fontouradev/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fontouradev.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
