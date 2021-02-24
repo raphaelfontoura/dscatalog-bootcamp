36279c7fd1762aaa0ab40759b6d93d31d7cb4efe
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-02-24 11:08:08 -0300

    implemented category dto

--- backend/src/main/java/com/fontouradev/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/fontouradev/dscatalog/services/CategoryService.java
@@ -1,13 +1,16 @@
 package com.fontouradev.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import com.fontouradev.dscatalog.entities.Category;
-import com.fontouradev.dscatalog.repositories.CategoryRepository;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.fontouradev.dscatalog.dto.CategoryDTO;
+import com.fontouradev.dscatalog.entities.Category;
+import com.fontouradev.dscatalog.repositories.CategoryRepository;
 
 
 
@@ -18,7 +21,16 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<Category> findAll(){
-        return repository.findAll();
+    public List<CategoryDTO> findAll(){
+        List<Category> list = repository.findAll();
+        
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+        
+//        List<CategoryDTO> listDto = new ArrayList<>();
+//        for (Category category : list) {
+//			listDto.add(new CategoryDTO(category));
+//		}
+        
+//        return listDto;
     }
 }
