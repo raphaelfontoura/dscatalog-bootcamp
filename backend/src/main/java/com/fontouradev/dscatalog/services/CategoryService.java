9075eba1ce09a29578fde1a05e48cb4b6e0615b6
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-02-24 10:46:30 -0300

    Data seeding for test environment

--- backend/src/main/java/com/fontouradev/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/fontouradev/dscatalog/services/CategoryService.java
@@ -7,6 +7,7 @@ import com.fontouradev.dscatalog.repositories.CategoryRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 
@@ -16,6 +17,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return repository.findAll();
     }
