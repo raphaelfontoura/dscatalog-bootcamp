69795c5a085542ac7dfeaf036fac57ad968367a2
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-03-04 22:14:43 -0300

    Add navbar and styles

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import './core/assets/styles/custom.scss'
+import './app.scss';
+import Navbar from "./core/components/Navbar";
 
 const App = () => {
 
 
     return (
-        <h1>Hello world!</h1>
+        <Navbar />
     );
 }
 
