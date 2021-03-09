3650b1188b3fbb07323a628e4bbe6e2d64534f6f
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-03-09 17:18:26 -0300

    Finish product list layout

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -6,7 +6,7 @@ import "./sytles.scss";
 
 const Home = () => (
     <div className="home-container">
-        <div className="row home-content">
+        <div className="row home-content card-base border-radius-20">
             <div className="col-6">
                 <h1 className="text-title">
                     Conheça o melhor <br />
