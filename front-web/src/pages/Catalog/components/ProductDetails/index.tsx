baeca8714bfb72d315a6fee53bd3b32fc02a250a
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-03-10 14:45:13 -0300

    Products Details page finished

--- front-web/src/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -1,4 +1,8 @@
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import { ReactComponent as ArrowIcon } from "../../../../core/assets/images/arrow.svg";
+import { ReactComponent as ProductImage } from "../../../../core/assets/images/product.svg";
+import ProductPrice from "../../../../core/components/ProductPrice";
 import "./styles.scss";
 
 type ParamsType = {
@@ -7,10 +11,34 @@ type ParamsType = {
 
 const ProductDetails = () => {
     const { productId } = useParams<ParamsType>();
-    
+
     return (
         <div className="product-details-container">
-            <h1>Product details {productId}</h1>
+            <div className="card-base border-radius-20 product-details">
+                <Link to="/products" className="product-details-goback">
+                    <ArrowIcon className="icon-goback" />
+                    <h1 className="text-goback">voltar</h1>
+                </Link>
+                <div className="row">
+                    <div className="col-6 pr-5">
+                        <div className="product-details-card text-center">
+                            <ProductImage className="product-details-image" />
+                        </div>
+                        <h1 className="product-details-name">
+                            Computador Desktop - Intel Core i7
+                        </h1>
+                        <ProductPrice price="2.779,00"/>
+                    </div>
+                    <div className="col-6 product-details-card">
+                        <h1 className="product-description-title">Descrição do produto</h1>
+                        <p className="product-description-text">
+                            Seja um mestre em multitarefas com a capacidade para exibir quatro aplicativos simultâneos na tela.
+                            A tela está ficando abarrotada? Crie áreas de trabalho virtuais para obter mais espaço e trabalhar com os itens que você deseja.
+                            Além disso, todas as notificações e principais configurações são reunidas em uma única tela de fácil acesso.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
