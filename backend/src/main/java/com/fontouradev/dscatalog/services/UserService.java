3c994260312ce1e5773982a40f3f6815e9ecc9b2
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-04-20 23:08:30 -0300

    Implemented OAuth2 authorization server

--- backend/src/main/java/com/fontouradev/dscatalog/services/UserService.java
+++ backend/src/main/java/com/fontouradev/dscatalog/services/UserService.java
@@ -4,10 +4,14 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,7 +28,9 @@ import com.fontouradev.dscatalog.services.exceptions.DatabaseException;
 import com.fontouradev.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+	
+	private static Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
 	
 	@Autowired
 	private UserRepository repository;
@@ -90,6 +96,17 @@ public class UserService {
 			Role role = roleRepositoy.getOne(roleDto.getId());
 			entity.getRoles().add(role);
 		}
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByEmail(username);
+		if (user == null) {
+			logger.error("User not found: " +  username);
+			throw new UsernameNotFoundException("Email not found.");
+		}
+		logger.info("user found: " +  username);
+		return user;
 	}
 	
 }
