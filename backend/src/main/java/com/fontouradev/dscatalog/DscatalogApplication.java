f794cac167e6503984b00f0ec9954150f071bf29
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-06-01 09:43:26 -0300

    Implemented Amazon S3 Service and excuted upload test

--- backend/src/main/java/com/fontouradev/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/fontouradev/dscatalog/DscatalogApplication.java
@@ -1,13 +1,23 @@
 package com.fontouradev.dscatalog;
 
+import com.fontouradev.dscatalog.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DscatalogApplication {
+public class DscatalogApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("/home/raphael/Pictures/Sea-1.jpg");
+	}
 }
