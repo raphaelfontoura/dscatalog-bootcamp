6f43c0a26ef969a0ac2eb57dae122841acc9a4c6
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-12-13 12:13:24 -0300

    Add auth background

--- front-web/src/core/utils/auth.ts
+++ front-web/src/core/utils/auth.ts
@@ -1,4 +1,4 @@
-import jwtDecode from 'jwt-decode';
+import jwtDecode from "jwt-decode";
 
 export const CLIENT_ID = "dscatalog";
 export const CLIENT_SECRET = "dscatalog123";
@@ -12,45 +12,48 @@ type LoginResponse = {
   UserId?: number;
 };
 
-export type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
+export type Role = "ROLE_OPERATOR" | "ROLE_ADMIN";
 
 type AccessToken = {
   exp: number;
   username: string;
   authorities: Role[];
-}
+};
 
 export const saveSessionData = (loginResponse: LoginResponse) => {
   localStorage.setItem("authData", JSON.stringify(loginResponse));
 };
 
-
 export const getSessionData = () => {
-  const sessionData = localStorage.getItem("authData") ?? '{}';
+  const sessionData = localStorage.getItem("authData") ?? "{}";
   return JSON.parse(sessionData) as LoginResponse;
-}
+};
 
 export const getAccessTokenDecoded = () => {
   const sessionData = getSessionData();
 
-  const tokenDecoded = jwtDecode(sessionData.access_token);
-  return tokenDecoded as AccessToken;
-}
+  try {
+    const tokenDecoded = jwtDecode(sessionData.access_token);
+    return tokenDecoded as AccessToken;
+  } catch (error) {
+    return {} as AccessToken;
+  }
+};
 
 export const isTokenValid = () => {
   const { exp } = getAccessTokenDecoded();
   return Date.now() <= exp * 1000;
-}
+};
 
 export const isAuthenticated = () => {
   const sessionData = getSessionData();
   return sessionData.access_token && isTokenValid();
-}
+};
 
 export const isAllowedByRole = (routeRoles: Role[] = []) => {
   if (routeRoles.length === 0) {
     return true;
   }
   const { authorities } = getAccessTokenDecoded();
-  return routeRoles.some(role => authorities.includes(role));
-}
+  return routeRoles.some((role) => authorities?.includes(role));
+};
