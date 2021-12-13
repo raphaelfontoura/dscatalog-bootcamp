af2016c8d27111906c3ea6934c1773f94fbffdc5
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-12-13 11:55:15 -0300

    Add allowed roles on components

--- front-web/src/pages/Admin/components/Navbar/index.tsx
+++ front-web/src/pages/Admin/components/Navbar/index.tsx
@@ -1,3 +1,4 @@
+import { isAllowedByRole } from "core/utils/auth";
 import { NavLink } from "react-router-dom";
 import "./styles.scss"
 
@@ -10,9 +11,11 @@ const Navbar = () => (
             <li>
                 <NavLink to="/admin/categories" className="admin-nav-item">Minhas Categorias</NavLink>
             </li>
-            <li>
+            {isAllowedByRole(['ROLE_ADMIN']) && (
+                <li>
                 <NavLink to="/admin/users" className="admin-nav-item">Meus Usuários</NavLink>
             </li>
+            )}
         </ul>
     </nav>
 );
