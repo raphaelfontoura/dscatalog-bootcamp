1af565895a00e1bffdce79a8f9c480f0992da7cb
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-12-21 17:29:41 -0300

    Fix ProductFilters

--- front-web/src/core/components/ProductFilters/index.tsx
+++ front-web/src/core/components/ProductFilters/index.tsx
@@ -6,21 +6,25 @@ import { makeRequest } from 'core/utils/request';
 import { Category } from 'core/types/Category';
 
 
-export type FilterForm = {
-  name?: string;
-  categoryId?: number;
-}
-
 type Props = {
-  onSearch: (filter: FilterForm) => void;
+  name?: string;
+  category?: Category;
+  handleChangeName: (value: string) => void;
+  handleChangeCategory: (category: Category) => void;
+  clearFilters: () => void;
 }
 
-const ProductFilters = ({ onSearch }: Props) => {
+const ProductFilters = ({
+  name,
+  handleChangeName,
+  handleChangeCategory,
+  clearFilters,
+  category
+}: Props) => {
 
   const [isLoadingCategories, setIsLoadingCategories] = useState(false);
   const [categories, setCategories] = useState<Category[]>([]);
-  const [name, setName] = useState('');
-  const [category, setCategory] = useState<Category>();
+
 
   useEffect(() => {
     setIsLoadingCategories(true);
@@ -29,21 +33,7 @@ const ProductFilters = ({ onSearch }: Props) => {
       .finally(() => setIsLoadingCategories(false));
   }, []);
 
-  const handleChangeName = (name: string) => {
-    setName(name);
-    onSearch({ name, categoryId: category?.id });
-  }
 
-  const handleChangeCategory = (category: Category) => {
-    setCategory(category);
-    onSearch({ name, categoryId: category?.id });
-  }
-
-  const clearFilters = () => {
-    setCategory(undefined);
-    setName('');
-    onSearch({ name: '', categoryId: undefined})
-  }
 
   return (
     <div className="card-base product-filters-container">
@@ -71,10 +61,10 @@ const ProductFilters = ({ onSearch }: Props) => {
         onChange={value => handleChangeCategory(value as Category)}
         isClearable
       />
-      <button 
+      <button
         className="btn btn-outline-secondary border-radius-10"
         onClick={clearFilters}
-        >
+      >
         LIMPAR FILTRO
       </button>
     </div>
--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,7 +1,7 @@
 import './styles.scss';
 
 import Pagination from 'core/components/Pagination';
-import ProductFilters, { FilterForm } from 'core/components/ProductFilters';
+import ProductFilters from 'core/components/ProductFilters';
 import { ProductsResponse } from 'core/types/Product';
 import { makeRequest } from 'core/utils/request';
 import { useCallback, useEffect, useState } from 'react';
@@ -9,19 +9,23 @@ import { Link } from 'react-router-dom';
 
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import ProductCard from './components/ProductCard';
+import { Category } from 'core/types/Category';
+
 
 const Catalog = () => {
 
   const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
   const [isLoading, setIsLoading] = useState(false);
   const [activePage, setActivePage] = useState(0);
+  const [name, setName] = useState('');
+  const [category, setCategory] = useState<Category>();
 
-  const getProducts = useCallback((filter?: FilterForm) => {
+  const getProducts = useCallback(() => {
     const params = {
       page: activePage,
       linesPerPage: 6,
-      name: filter?.name,
-      categoryId: filter?.categoryId
+      name,
+      categoryId: category?.id
     }
 
     setIsLoading(true);
@@ -34,11 +38,29 @@ const Catalog = () => {
     } catch (err) {
       console.log(err);
     }
-  }, [activePage]);
+  }, [activePage, name, category]);
 
   useEffect(() => {
     getProducts();
   }, [getProducts]);
+
+
+
+  const handleChangeName = (name: string) => {
+    setActivePage(0);
+    setName(name);
+  }
+
+  const handleChangeCategory = (category: Category) => {
+    setActivePage(0);
+    setCategory(category);
+  }
+
+  const clearFilters = () => {
+    setActivePage(0);
+    setCategory(undefined);
+    setName('');
+  }
 
   return (
     <div className="catalog-container">
@@ -46,7 +68,13 @@ const Catalog = () => {
         <h1 className="catalog-title">
           Catálogo de produtos
         </h1>
-        <ProductFilters onSearch={filter => getProducts(filter)} />
+        <ProductFilters
+          name={name}
+          category={category}
+          handleChangeCategory={handleChangeCategory}
+          handleChangeName={handleChangeName}
+          clearFilters={clearFilters}
+        />
       </div>
 
       <div className="catalog-products">
